636b0d5834fd3eb060482c39b6dc807e8a61efeb
Hébert, François <francois.hebert@elekta.com>
2019-06-20 14:00:13 -0400

    Working fitting model script

--- DogsCatsClassificationKeras.py
+++ DogsCatsClassificationKeras.py
@@ -8,27 +8,21 @@ Created on Wed Apr 24 23:08:16 2019
 
 import numpy as np # linear algebra
 import argparse
-
-#from sklearn.model_selection import train_test_split
-#from sklearn.utils import shuffle
+import pandas as pd
 
 import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense#, Activation
 
+
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
 def SaveHistory(csvFilename, history):
     hist_df = pd.DataFrame(history.history) 
     hist_csv_file = csvFilename
     with open(hist_csv_file, mode='w') as f:
         hist_df.to_csv(f)
-
-
-#------------------------------------------------------------------------------
-# Variables
-#------------------------------------------------------------------------------
-EPOCHS = 200
-BATCH_SIZE = 32
-LR = 1e-6
 #------------------------------------------------------------------------------
 
 
@@ -42,37 +36,34 @@ def CreateModel(dataSize):
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(64, kernel_size=3, activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(1, activation='sigmoid'))
+    
+    return model
 #------------------------------------------------------------------------------
 
 
-##------------------------------------------------------------------------------
-## Fit the model
-##------------------------------------------------------------------------------
-#batch_size = [32]#,64,128,256,512]
-#
-## compile model using accuracy to measure model performance
-#optimizer = keras.optimizers.Adam(lr=LR)
-#model.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics=['accuracy'])
-#model.summary()
-#
-#for bsize in batch_size:
-#    print (bsize)
-#    historyFileName = "DogsCatsClassificationKerasHistoryBatchSize" + str(bsize) + ".csv"
-#    print (historyFileName)
-#
-#    history = model.fit(train_data,
-#                        train_label, 
-#                        epochs = EPOCHS, 
-#                        verbose = 2,
-#                        batch_size = bsize, 
-#                        validation_data = (valid_data, valid_label))
-#    
-#    # convert the history.history dict to a pandas DataFrame:     
-#    SaveHistory(historyFileName, history)
-#    #model.save("DogsCatsClassificationKerasModel.h5")
-##------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# Fit the model
+#------------------------------------------------------------------------------
+def FitModel(model, training, learningRate, epochs, batchSize, validation = None):
+    
+    # Optimizer
+    optimizer = keras.optimizers.Adam(lr=learningRate)
+    model.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    history = model.fit(training['data'],
+                        training['label'], 
+                        epochs = epochs, 
+                        verbose = 2,
+                        batch_size = batchSize, 
+                        validation_data = validation)
+    
+    return history
+#------------------------------------------------------------------------------
     
     
 #------------------------------------------------------------------------------
@@ -88,15 +79,60 @@ def main():
     parser.add_argument('-v', '--validationDataFile',
                         type = str,
                         nargs = '?',
-                        help = 'numpy file containing validation data.')    
+                        help = 'numpy file containing validation data.')
+    parser.add_argument('-m', '--modelFile',
+                        type = str,
+                        nargs = '?',
+                        help = 'file to store the model.')
+    parser.add_argument('-t', '--trainingHistoryFile',
+                        type = str,
+                        nargs = '?',
+                        help = 'file to store the training history.')
+    parser.add_argument('-e', '--epochs',
+                        type = int,
+                        default = 200,
+                        help = 'Number of epochs for trainig data.')
+    parser.add_argument('-lr', '--learningRate',
+                        type = float,
+                        default = 1e-6,
+                        help = 'Learning rate for training.')
+    parser.add_argument('-b', '--batchSize',
+                        type = int,
+                        default = 64,
+                        help = 'Learning rate for training.')
     args = parser.parse_args()
     
-    # Get data shape
+    # Get training data shape
     trainingData = np.load(args.trainingDataFile)
-    dataSize = trainingData.item().get('data')[0].shape
+    training = {
+            'data': trainingData.item().get('data') ,
+            'label': trainingData.item().get('label')}
+    dataSize = training['data'][0].shape
     
+    # Get validationdata
+    validation = None
+    if args.validationDataFile is not None:
+        data = np.load(args.validationDataFile)
+        validation = (data.item().get('data'), data.item().get('label'))
+        
     # Create the model
-    CreateModel(dataSize)
+    model = CreateModel(dataSize)
+    
+    # Fit model
+    trainingHistory = FitModel(model, 
+                               training, 
+                               args.learningRate, 
+                               args.epochs, 
+                               args.batchSize, 
+                               validation = validation)
+    
+    # save history
+    if args.trainingHistoryFile is not None:
+        SaveHistory(args.trainingHistoryFile, trainingHistory)
+        
+    # save model
+    if args.modelFile is not None:
+        model.save(args.modelFile)
 #------------------------------------------------------------------------------
 
 
